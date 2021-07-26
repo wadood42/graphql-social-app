2271c613a0242c9d0e8dd08f74fe67fb25d49920
Abdul <wbehzad12@gmail.com>
2021-07-25 22:22:11 -0500

    added menu bar

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import MenuBar from "./components/MenuBar";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-      </Switch>
+      <div className='main-container'>
+        <MenuBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </Switch>
+      </div>
     </Router>
   );
 }
